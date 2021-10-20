976819614c6304627be6119e21758d1a07d0d83f
vimalshah25git config --global user.email vimalshah2510@gmail.com <vimalshah2510@gmail.comgit config --global lgit config --global-l!git commit -m first>
2021-10-20 14:16:10 +0100

    operators java

---
+++ bionerry.java
@@ -0,0 +1,13 @@
+public class bionerry {
+    public static void main(String[] args) {
+
+
+        int a = 60;  // "60 = 0011 1100 " //
+        int b = 13;  // "13 = 0000 1101 " //
+        int c = 0;
+        c = a&b;
+
+        System.out.println(" a & b =" + c );
+
+    }
+}
---
+++ operators.java
@@ -0,0 +1,62 @@
+public class operators  {
+    public static void main(String[] args) {
+        float a = 10;
+        float b = 20;
+        float  c;
+
+        System.out.println("addition of numbers ");
+        System.out.println(a+b);
+
+        System.out.println("substraction of numbers  ");
+        System.out.println(a-b);
+
+        System.out.println("multiplication of numbers ");
+        System.out.println(a*b);
+
+        System.out.println("modulus of numbers");
+        System.out.println(a%b);
+
+        System.out.println("division of numbers ");
+        System.out.println(a/b);
+
+        System.out.println("increments of numbers ");
+        System.out.println(++a);
+
+        System.out.println("increaments of numbers ");
+        System.out.println(++b);
+
+        System.out.println("decreases of numbers ");
+        System.out.println(--a);
+        System.out.println("decreases of numbers " );
+        System.out.println(--b);
+
+        System.out.println("equal to ");
+        System.out.println(1==b);
+
+        System.out.println("!=not equal to " );
+        System.out.println(a!=b);
+
+        System.out.println("a greater then" );
+        System.out.println(a > b );
+
+        System.out.println("a is less then b ");
+        System.out.println(a<b);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
